95974174886f62ba3be2e77e33eb1a0c032b3471
Samuel Alves <alvess7221@gmail.com>
2024-03-06 08:14:37 -0300

    commit 8 - 06/03/2024

--- LISTA 1 - EXPRESSOẼS EM C/questao 7.c
+++ LISTA 1 - EXPRESSOẼS EM C/questao 7.c
@@ -3,7 +3,18 @@
 int main(){
     int h, m, s, segundos;
 
-    puts("Digite o ");
+    puts("Horas: ");
+    scanf("%d", & h);
+
+    puts("Minutos: ");
+    scanf("%d", & m);
+
+    puts("Segundos: ");
+    scanf("%d", & s);
+
+    segundos = (3600 * h) + (60 * m) + s;
+
+    printf("A quantidade em segundos é: %d segunos. \n", segundos);
 
     return 0;
 }
