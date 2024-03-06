06d8373d47b20fe5eea539fa7aff977c1e7f7431
Samuel Alves <alvess7221@gmail.com>
2024-03-06 08:34:31 -0300

    commit 8.1 - 06/03/2024

--- LISTA 1 - EXPRESSOẼS EM C/questao 8.c
+++ LISTA 1 - EXPRESSOẼS EM C/questao 8.c
@@ -2,7 +2,15 @@
 #include <math.h>
 
 int main(){
-    
+    double x, y = 2;
+    double resultado;
+
+    puts("Digite um número para elevalo ao quadrado: ");
+    scanf("%lf", & x);
+
+    resultado = pow(x, y); 
+
+    printf("O número %lf elevado ao quadrado é %lf \n", x, resultado);
 
     return 0;
 }
