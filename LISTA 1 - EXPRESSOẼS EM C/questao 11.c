a6ee2e88e5ace0ab9a1daa30425f2cdbe4655cea
Samuel Alves <alvess7221@gmail.com>
2024-03-07 07:38:29 -0300

    commit 9.1 - 07/03/2024

--- LISTA 1 - EXPRESSOẼS EM C/questao 11.c
+++ LISTA 1 - EXPRESSOẼS EM C/questao 11.c
@@ -1,2 +1,17 @@
 #include <stdio.h>
 
+int main(){
+    double real, dolar, total;
+
+    printf("Valor da cotação atual do dolar para real: $"); 
+    scanf("%lf", & dolar);
+
+    printf("Quantidade de reais para converter em dolar: R$"); 
+    scanf("%lf", & real);
+
+    total = real * dolar;
+
+    printf("Valor convertido para dolar será: $%.2lf \n", total);
+
+    return 0;
+}
