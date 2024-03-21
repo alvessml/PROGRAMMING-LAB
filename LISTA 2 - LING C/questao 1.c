d478ed310718e6edfc32d48cac39f8feefb184c6
Samuel Alves <alvess7221@gmail.com>
2024-03-21 09:51:10 -0300

    commit 12 - 21/03/2024

--- LISTA 2 - LING C/questao 1.c
+++ LISTA 2 - LING C/questao 1.c
@@ -1,33 +1,46 @@
 #include <stdio.h>
 
 int main(){
-    int ent_usuário;
+    unsigned char ent_usuário, ent;
 
-    puts("Digite um número de 1 á 7 para imprimir o dia da semana: ");
-    ent_usuário = getchar();     
-    
-    switch (ent_usuário)
-        {
-        case '0':
-            break;
-        case '1':
-            puts("Dia da semana é: Domingo. \n");
-        case '2':
-            puts("Dia da semana é: Segunda-feira. \n");
-        case '3':
-            puts("Dia da semana é: Terça-feira. \n");
-        case '4':
-            puts("Dia da semana é: Quarta-feira. \n");
-        case '5':
-            puts("Dia da semana é: Quinta feira. \n");
-        case '6':
-            puts("Dia da semana é: Sexta-feira. \n");
-        case '7':
-            puts("Dia da semana é: Sábado. \n");
+    // puts("Digite um número de 1 á 7 para imprimir o dia da semana: ");
+    // ent_usuário = getchar();     
 
-        default:
-            puts("Erro. Número de dia não válido. \n");
-            break;
+    while (1){
+        puts("Digite um número de 1 á 7 para imprimir o dia da semana: ");
+
+        ent_usuário = getchar();    
+        
+        switch (ent_usuário)
+            {
+            case '0':
+                return 0;
+            case '1':
+                puts("Dia da semana é: Domingo. \n");
+                continue;
+            case '2':
+                puts("Dia da semana é: Segunda-feira. \n");
+                continue;
+            case '3':
+                puts("Dia da semana é: Terça-feira. \n");
+                continue;
+            case '4':
+                puts("Dia da semana é: Quarta-feira. \n");
+                continue;
+            case '5':
+                puts("Dia da semana é: Quinta feira. \n");
+                continue;
+            case '6':
+                puts("Dia da semana é: Sexta-feira. \n");
+                continue;
+            case '7':
+                puts("Dia da semana é: Sábado. \n");
+                continue;
+
+            default:
+                puts("Erro. Número de dia não válido. \n");
+                continue;
+            }
         }
     return 0;
 }
