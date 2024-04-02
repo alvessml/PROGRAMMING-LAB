a2ebf0e06623dfd834bc5e20f6c640c0378afd11
Samuel Alves <alvess7221@gmail.com>
2024-04-02 05:36:00 -0300

    commit 15.1 -  02/04/2024

--- teste.c
+++ teste.c
@@ -3,12 +3,9 @@
 #include <time.h>
 
 #define TAM 100
-#define MIN 0
-#define MAX 20
+#define MAX 100
 
 int main(){
-    printf("Gerando valores aleatórios e descobrindo o maior e menor valor \n");
-
     int x[TAM];
 
     // sdram serve para passar uma semente qualquer para gerar valores aleatórios apartir da semente, no
@@ -17,33 +14,29 @@ int main(){
     srand(time(NULL));
 
     //Preencher o vetor
-    for(int k=0; k<TAM; k++){
+    for (int k=0; k<TAM; k++){
                         /*obs abaixo: serve para obeter um limite dos valores aleatório gerados, por meio
                         do resto da divisão de qualquer número aleatório gerado pela rand.*/
         x[k] = rand() % (MAX+1);
     }
-    
-    
-    // Imprimindo o vetor
-    for(int k=0; k < TAM; k++){
+
+    // Imprimir o vetor
+    for (int k=0; k<TAM; k++){
         printf("%d, ",x[k]);
-        if(k < TAM){
-                printf(", ");
-            } else{
-                printf(". \n");
-        }
     }
-
-
-    // Processamento
-    int maior = x[0], menor = x[0];
-    for(int k=1; k < 100; k++){
-        x[k] = rand() & (MAX+1);
-        maior = (maior < x[k])? x[k]: maior;
-        menor = (menor > x[k])? x[k]: menor;
-    }
-
     printf("\n");
+
+    //Processamento
+    int maior = x[0], menor = x[0];
+    for (int k=0; k<TAM; k++){
+        maior = (maior < x[k])?x[k]:maior;
+        menor = (menor > x[k])?x[k]:menor;
+    }
+
+    // Saída: 
+    printf("\t\tMaior: %d\n",maior);
+    printf("\t\tMenor: %d\n",menor);
+    
 
     return 0;
 }
