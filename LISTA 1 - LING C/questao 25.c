3cb412a471b244096026418c02f321e3d355e482
Samuel Alves <alvess7221@gmail.com>
2024-04-15 08:53:51 -0300

    15/04/2024 -- 20.2

--- LISTA 1 - LING C/questao 25.c
+++ LISTA 1 - LING C/questao 25.c
@@ -7,13 +7,11 @@ int main(){
     printf("Digite quantos segundos para converte em (hh/mm/ss): ");
     scanf("%d", & x);
 
-    horas = x / 60;
+    horas  =  x / 3600;
+    minutos = (x % 3600) / 60;
+    segundos = x % 60;
 
-    minutos = (x % 60) / 60;
-    for(int k = 0; k < 60; )
-        segundos = x / 60;
-
-    printf("\n%d segundos em (hh/mm/ss): %d:%d:%d \n", x, horas, minutos, segundos);
+    printf("\n%d segundos em (hh/mm/ss): %2.2d:%2.2d:%2.2d \n", x, horas, minutos, segundos);
 
     return 0;
 }
