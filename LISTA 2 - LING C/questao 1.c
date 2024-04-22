9f76f049a82af81b250d6a678362ea86a56ec943
Samuel Alves <alvess7221@gmail.com>
2024-04-22 09:13:26 -0300

    21 -- 22/04/2024

--- LISTA 2 - LING C/questao 1.c
+++ LISTA 2 - LING C/questao 1.c
@@ -1,45 +1,57 @@
 #include <stdio.h>
 
 int main(){
-    unsigned char ent_usuário, ent;
+    unsigned char ent_usuário;   
 
-    // puts("Digite um número de 1 á 7 para imprimir o dia da semana: ");
-    // ent_usuário = getchar();     
+    puts("Digite um número de 1 á 7 para imprimir o dia da semana: ");   
+    
+    while (1){    
+        ent_usuário = getchar();
+        getchar();
 
-    while (1){
-        puts("Digite um número de 1 á 7 para imprimir o dia da semana: ");
+        if (ent_usuário >= '0' && ent_usuário <= '7'){
+            switch (ent_usuário)
+                {
+                case '0':
+                    return 0;
 
-        ent_usuário = getchar();    
-        
-        switch (ent_usuário)
-            {
-            case '0':
-                return 0;
-            case '1':
-                puts("Dia da semana é: Domingo. \n");
-                continue;
-            case '2':
-                puts("Dia da semana é: Segunda-feira. \n");
-                continue;
-            case '3':
-                puts("Dia da semana é: Terça-feira. \n");
-                continue;
-            case '4':
-                puts("Dia da semana é: Quarta-feira. \n");
-                continue;
-            case '5':
-                puts("Dia da semana é: Quinta feira. \n");
-                continue;
-            case '6':
-                puts("Dia da semana é: Sexta-feira. \n");
-                continue;
-            case '7':
-                puts("Dia da semana é: Sábado. \n");
-                continue;
+                case '1':
+                    puts("Dia da semana é: Domingo.");
+                    printf("------------------------------\n");
+                    break;
 
-            default:
-                puts("Erro. Número de dia não válido. \n");
-                continue;
+                case '2':
+                    puts("Dia da semana é: Segunda-feira.");
+                    printf("------------------------------\n");
+                    break;
+                    
+                case '3':
+                    puts("Dia da semana é: Terça-feira.");
+                    printf("------------------------------\n");
+                    break;                
+                case '4':
+                    puts("Dia da semana é: Quarta-feira.");
+                    printf("------------------------------\n");
+                    break;
+
+                case '5':
+                    puts("Dia da semana é: Quinta feira.");
+                    printf("------------------------------\n");
+                    break;
+
+                case '6':
+                    puts("Dia da semana é: Sexta-feira.");
+                    printf("------------------------------\n");
+                    break;
+
+                case '7':
+                    puts("Dia da semana é: Sábado.");
+                    printf("------------------------------\n");
+                    break;
+                }
+        }else{
+                printf("Número de dia não válido!\n");
+                printf("------------------------------\n");
             }
         }
     return 0;
