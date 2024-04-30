84d3e456e5580ca02e80b4436c120371e02e3e9c
Samuel Alves <alvess7221@gmail.com>
2024-04-30 09:22:13 -0300

    commit 23.1 - 30/04/2024

--- LISTA 2 - LING C/questao 6.c
+++ LISTA 2 - LING C/questao 6.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 
 int main(){
-    while(1){
-        unsigned char usuario;
+    int votacao;
+    while(votacao == 1){
+        int usuario, votoPedro = 0, votoRenata = 0, votoTotal = 0, votoBranco = 0, votoNulo = 0;
 
-        puts("Digite 5 para votar em Paulo e 7 para votar na Renata \n(obs: 0 - voto em branco; Quarquer tecla a mais positivo - voto nulo): ");
-        scanf("%", & usuario);
+        puts("Digite 5 para votar em Pedro e 7 para votar na Renata (obs: 0 - voto em branco; Quarquer tecla a mais positivo - voto nulo): ");
+        scanf("%d", & usuario);
 
-        switch (usuario)
-        {
-        case :
-            /* code */
-            break;
-        
-        default:
-            break;
+        if(usuario < 0){
+            votacao = 0;
+        } else if(usuario == 5){
+            votoPedro += 1;
+        } else if(usuario == 7){
+            votoRenata += 1;
+        } else if(usuario == 0){
+            votoBranco += 1;
+        } else if(usuario != 7 && usuario != 5 && usuario > 0){
+            votoNulo += 1;
         }
     }
 
