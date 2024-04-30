2dd78069339a8aa58c507dbcd829c69e96e3b979
Samuel Alves <alvess7221@gmail.com>
2024-04-30 08:21:05 -0300

    commit 23 - 30/04/2024

---
+++ LISTA 2 - LING C/questao 6.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    while(1){
+        unsigned char usuario;
+
+        puts("Digite 5 para votar em Paulo e 7 para votar na Renata \n(obs: 0 - voto em branco; Quarquer tecla a mais positivo - voto nulo): ");
+        scanf("%", & usuario);
+
+        switch (usuario)
+        {
+        case :
+            /* code */
+            break;
+        
+        default:
+            break;
+        }
+    }
+
+    return 0;
+}
