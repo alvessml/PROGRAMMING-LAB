facb404c8e71044e9d71eac33642320998395162
Samuel Alves <alvess7221@gmail.com>
2024-05-02 08:00:46 -0300

    commit 24 - 02/05/2024

--- LISTA 2 - LING C/questao 6.c
+++ LISTA 2 - LING C/questao 6.c
@@ -1,25 +1,85 @@
 #include <stdio.h>
 
 int main(){
-    int votacao;
-    while(votacao == 1){
-        int usuario, votoPedro = 0, votoRenata = 0, votoTotal = 0, votoBranco = 0, votoNulo = 0;
+    int usuario, votoPedro = 0, votoRenata = 0, votoBranco = 0, votoNulo = 0, votoTotal = 0;
+    char confirmaVoto;
 
-        puts("Digite 5 para votar em Pedro e 7 para votar na Renata (obs: 0 - voto em branco; Quarquer tecla a mais positivo - voto nulo): ");
+    printf("URNA ELETRÕNICA:\n");
+    while(1){
+        puts("Digite:\n5 - para votar em Pedro;\n7 - para votar em Renata;\n0 - para votar em branco;\nDigite qualquer número maior que 0 e diferente de 5 e 7 - para votar nulo;\nDigite qualquer número menor que 0 - para encerrar a votação:");
         scanf("%d", & usuario);
 
-        if(usuario < 0){
-            votacao = 0;
+        if(usuario == 0){
+            fflush(stdin);
+            puts("\n\nVocê confirmar seu voto(sim - s; não - n)? ");
+            scanf(" %c", & confirmaVoto);
+            if(confirmaVoto == 's'){
+                votoBranco += 1;
+                votoPedro += 1;
+                votoRenata += 1;
+                votoTotal += 1;
+            } else {
+                printf("\n========================================\n");  
+                continue;
+            }
+        } else if(usuario > 0 && usuario != 5 && usuario != 7){
+            fflush(stdin);
+            puts("\nVocê confirmar seu voto(sim - s; não - n)? ");
+            scanf(" %c", & confirmaVoto);
+            if(confirmaVoto == 's'){
+                votoNulo += 1;
+                votoTotal += 1;
+            } else {
+                printf("\n========================================\n");  
+                continue;
+            }
         } else if(usuario == 5){
-            votoPedro += 1;
+            fflush(stdin);
+            puts("\n\nVocê confirmar seu voto(sim - s; não - n)? ");
+            scanf(" %c", & confirmaVoto);
+            if(confirmaVoto == 's'){
+                votoPedro += 1;
+                votoTotal += 1;
+            } else {
+                printf("\n========================================\n");  
+                continue;
+            }
         } else if(usuario == 7){
-            votoRenata += 1;
-        } else if(usuario == 0){
-            votoBranco += 1;
-        } else if(usuario != 7 && usuario != 5 && usuario > 0){
-            votoNulo += 1;
+            fflush(stdin);
+            puts("\n\nVocê confirmar seu voto(sim - s; não - n)? ");
+            scanf(" %c", & confirmaVoto);
+            if(confirmaVoto == 's'){
+                votoRenata += 1;
+                votoTotal += 1;
+            } else {
+                printf("\n========================================\n");  
+                continue;
+            }
+        } else {
+            break;;
         }
+        printf("\n========================================\n");        
     }
 
+    float porcentVotoPedro, porcentVotoRenata, porcentVotoBranco, porcentVotoNulo;
+
+    porcentVotoPedro = ((float)votoPedro/(float)votoTotal)*100;
+    porcentVotoRenata = ((float)votoRenata/(float)votoTotal)*100;
+    porcentVotoBranco = ((float)votoBranco/(float)votoTotal)*100;
+    porcentVotoRenata = ((float)votoNulo/(float)votoTotal)*100;
+
+    puts("\n\n\nPorcentagem de votos: ");
+    printf("\n%.2f%% de votos para o candidato Pedro.", porcentVotoPedro);
+    printf("\n%.2f%% de votos para a candidato Renata.", porcentVotoRenata);
+    printf("\n%.2f%% de votos em brancos.", porcentVotoBranco);
+    printf("\n%.2f%% de votos em nulos.", porcentVotoNulo);
+    
+    if(votoPedro > votoRenata){
+        puts("\n\nCANDIDATO ELEITO É PEDRO!");
+    } else if(votoRenata > votoPedro){
+        puts("\n\nCANDIDATO ELEITO É RENATA!");
+    } else{
+        printf("OS DOIS CANDIDATOS ESTÃO EMPATADOS EM PORCENTAGENS DE VOTOS COM %d%% PARA OS DOIS", votoPedro);
+    }
     return 0;
 }
