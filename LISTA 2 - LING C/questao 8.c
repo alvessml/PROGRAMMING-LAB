28fc2a4a30ae82c7b74238af38ec3fa4715fae87
Samuel Alves <alvess7221@gmail.com>
2024-05-20 08:04:09 -0300

    commit 27 - 20/05/2024

---
+++ LISTA 2 - LING C/questao 8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void calculo_prestacao()
+
+
+int main(){
+    float v_produto;
+
+    puts("Digite o valor do produto: ");
+    scanf("%f", & v_produto);
+
+    
+
+
+    return 0;
+}
