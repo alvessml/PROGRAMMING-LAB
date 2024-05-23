42eb87bba7c7c6fcf8a2fd66b50dc59019cac685
Samuel Alves <alvess7221@gmail.com>
2024-05-23 09:01:36 -0300

    commit 28.1 - 23/05/2024

--- LISTA 2 - LING C/questao 8.c
+++ LISTA 2 - LING C/questao 8.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
-void calculo_prestacao(){
-    
-}
-
 
 int main(){
-    float v_produto;
+    float v_produto, entrada, prestacao;
 
     puts("Digite o valor do produto: ");
     scanf("%f", & v_produto);
 
-    
+    prestacao = (v_produto / 3);
+    entrada = v_produto - (2 * prestacao);
 
+    printf("\nO valor da entrada será %.2f e o valor das duas prestações será %d\n", entrada, prestacao);
 
     return 0;
 }
