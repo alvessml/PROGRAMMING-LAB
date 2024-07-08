84aca2abbfe5f528f2c1e7c18a97a5dea982bb10
Samuel Alves <alvess7221@gmail.com>
2024-07-08 09:19:02 -0300

    commit 32.2 - 08/07/2024

--- LISTA 2 - LING C/questao 12.c
+++ LISTA 2 - LING C/questao 12.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    
+    // ENTRADA
+    int valor;
+
+    printf("Digite um valor para mostrar a tabuada desse valor: ");
+    scanf("%d", & valor);
+
+
+    // PROCESSAMENTO / SAÍDA
+    puts("TABUADA DE ADIÇÃO: \n");
+    for(int i = 0; i <= 10; i++){
+        printf("%d + %d = %d\n", valor, i, valor + i);
+    }
 
     return 0;
 }
