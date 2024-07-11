cc453ff89077fa08e8d4805a48ed72b2dd20346d
Samuel Alves <alvess7221@gmail.com>
2024-07-11 17:46:05 -0300

    commit 33.2 - 11/07/2024

---
+++ LISTA 2 - LING C/questao 13.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+int main(){
+    // ENTRADA DE DADOS 
+    int faixa1, faixa2;
+
+    puts("Digite um número qualquer: ");
+    scanf("%d", & faixa1);
+
+    puts("Digite um número qualquer: ");
+    scanf("%d", & faixa2);
+
+
+    // PROCESSAMENTO
+    int somatorioDePares = 0;
+    
+    if(faixa1 > faixa2){
+        for(int i = faixa1; i <= faixa2; i++){
+            if(i % 2 == 0){
+                somatorioDePares += i;
+            }
+        }
+    } else{
+        for(int i = faixa2; i <= faixa1; i--){
+            if(i % 2 == 0){
+                somatorioDePares += i;
+            }
+        }
+    }
+
+
+    // SAÍDA
+    printf("O somátório dos pares dessa faixa entre esses dois números dados é %d\n", somatorioDePares);
+    return 0;
+}
