f6a8e684df104ae0d87a39108c1ba082388bd357
Samuel Alves <alvess7221@gmail.com>
2024-07-18 13:04:07 -0300

    commit 34.1 - 18/07/2024

--- LISTA 2 - LING C/questao 14.c
+++ LISTA 2 - LING C/questao 14.c
@@ -2,8 +2,36 @@
 
 
 int main(){ 
-    
+    // ENTRADA
+    int faixa1, faixa2;
 
+    puts("Digite um número qualquer: ");
+    scanf("%d", & faixa1);
+
+    puts("Digite outro número qualquer: ");
+    scanf("%d", & faixa2);
+
+
+    // PROCESSAMENTO
+    int quantDivTres;
+
+    if(faixa2 > faixa1){
+        for(int i = faixa1; i >= faixa2; i++){
+            if(i % 3 == 0){
+                quantDivTres += 1;
+            }
+        }
+    } else{
+        for(int i = faixa2; i >= faixa1; i++){
+            if(i % 3 == 0){
+                quantDivTres += 1;
+            }
+        }
+    }
+
+
+    // SAÍDA
+    printf("Existem %d números divisíveis por 3 nesta faixa. \n", quantDivTres);
 
     return 0;
 }
