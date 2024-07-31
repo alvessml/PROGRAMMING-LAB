92675c847c231067bd89db12b688286d493f163c
Samuel Alves <alvess7221@gmail.com>
2024-07-31 09:23:58 -0300

    commit - aula 31/07/2024

--- AULAS/PROJETOS/11. GERANDO UM VETOR QUE O USUARIO IRA DEFINIR O TAMNHO, APÓS IMPRIMIR/ex.c
+++ AULAS/PROJETOS/11. GERANDO UM VETOR QUE O USUARIO IRA DEFINIR O TAMNHO, APÓS IMPRIMIR/ex.c
@@ -7,18 +7,38 @@
 int main(){
     int *p = NULL;
     int k;
-
+    printf("Qunatidade de elementos: ");
     scanf("%d", & k);
 
-    p = malloc(k * sizeof(int));
+    // Aloca memória
+    // p = malloc(k * sizeof(int));
 
+    //OU
+
+    // p = (int*) malloc(k * sizeof(int))
+    // (int*) - Usa-se muito para documentar o código, visto que esclarece a saída int.
+
+    // if(!(p)){
+    //     puts("Memória indisponível!");
+    //     exit(1);
+    // }
+
+    // OU
+
+    if(!(p = malloc(k * sizeof(int)))){
+        puts("Memória indisponível!");
+        exit(1);
+    }
+
+
+    // Preenchendo o vetor
     srand(time(NULL));
     for(int i=0; i < k; i++){
-        p[i] = rand() % MX;
+        *(p+k) = rand() % MX;
     }
 
     for(int i=0; i < k; i++){
-        printf("%d", *p[i]);
+        printf("[%p] %d\n", p+i, *(p+i));
     }
 
     free(p);
