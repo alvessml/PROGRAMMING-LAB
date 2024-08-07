1ee7d644abce1b4b3bdaf661a01796c5fd782f54
Samuel Alves <alvess7221@gmail.com>
2024-08-07 07:20:48 -0300

    commit 37 - 07/08/2024

--- LISTA 2 - LING C/questao 19.c
+++ LISTA 2 - LING C/questao 19.c
@@ -10,19 +10,22 @@ int main(){
     printf("Digite outro número qualquer: ");
     scanf("%d", & faixa2);
     
-    int somatoriaDaFaixa;
+    int somatorioDaFaixa = 0;
     if(faixa2 > faixa1){
-       for(int i = faixa1; i >= faixa2; i++){
-            somatoriaDaFaixa += i;
+       for(int i = faixa1; i <= faixa2; i++){
+            somatorioDaFaixa += i;
        }
     } else{
-        for(int i = faixa2; i >= faixa1; i++){
-            somatoriaDaFaixa += i;
+        for(int i = faixa2; i <= faixa1; i++){
+            somatorioDaFaixa += i;
        }
     }
 
+    if(faixa2 > faixa1){
+        printf("Somatório da faixa entre %d e %d é %d.\n", faixa1, faixa2, somatorioDaFaixa);
+    } else{
+        printf("Somatório da faixa entre %d e %d é %d.\n", faixa2, faixa1, somatorioDaFaixa);
+    }
     
-
-    printf("")
     return 0;
 }
