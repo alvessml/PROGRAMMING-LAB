30432717eee309ba79b6a0a7d709cc6d906cb461
Samuel Alves <alvess7221@gmail.com>
2024-08-09 17:01:20 -0300

    commit 39.1 - 09/08/2024

--- LISTA 2 - LING C/questao 25.c
+++ LISTA 2 - LING C/questao 25.c
@@ -1,9 +1,26 @@
+// OBS: Não consegui fazer, essa é uma demostração do chatgpt
+
+
 #include <stdio.h>
 
+int main() {
+    int n;
+    double pi_aproximado = 0.0;
 
-int main(){
-    
+    printf("Termos\tValor aproximado de π\n");
+    printf("---------------------------------\n");
 
+    // Loop para calcular π para diferentes números de termos
+    for (n = 1; n <= 100; n++) {
+        // Calcular o termo n-ésimo
+        double termo = ((n % 2 == 0) ? -1 : 1) * 1.0 / (2 * (n - 1) + 1);
+        pi_aproximado += termo;
+        // Multiplicar por 4 para obter a aproximação de π
+        double pi_final = pi_aproximado * 4;
+
+        // Imprimir o número de termos e o valor aproximado de π
+        printf("%d\t%.15f\n", n, pi_final);
+    }
 
     return 0;
 }
