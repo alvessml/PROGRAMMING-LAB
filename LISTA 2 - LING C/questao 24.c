f08f44ced4e32b416e300b0c3d482c8c9977a5f6
Samuel Alves <alvess7221@gmail.com>
2024-08-09 13:29:53 -0300

    commit 39 - 09/08/2024

--- LISTA 2 - LING C/questao 24.c
+++ LISTA 2 - LING C/questao 24.c
@@ -2,7 +2,18 @@
 
 
 int main(){
+    int cateto1, cateto2, hipotenusa;
 
+    printf("Todas as triplas de Pitágoras não superiores a 500: \n");
+    for(cateto1 = 1; cateto1 <= 500; cateto1++){
+        for(cateto2 = cateto1; cateto2 <= 500; cateto2++){
+            for(hipotenusa = cateto2; hipotenusa <= 500; hipotenusa++){
+                if(cateto1 * cateto1 + cateto2 * cateto2 == hipotenusa * hipotenusa){
+                    printf("[%d, %d, %d]\n", cateto1, cateto2, hipotenusa);
+                }
+            }
+        }
+    }
 
     return 0;
 }
