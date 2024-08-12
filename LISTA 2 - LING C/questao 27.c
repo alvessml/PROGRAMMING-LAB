2fe5334ff69c89df747d34d182122832e5d09c76
Samuel Alves <alvess7221@gmail.com>
2024-08-12 04:49:18 -0300

    commit 40.1 - 12/08/2024

---
+++ LISTA 2 - LING C/questao 27.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    int x = 0;
+
+    int maior = 0;
+    int menor = 0;
+    while(1){
+        printf("Digite um número qualquer (obs: Para sair digite um número negativo par): ");
+        scanf("%d", & x);
+
+        if(x < 0 && x % 2 == 0){
+            break;
+        }
+        
+        if(x > maior){
+            maior = x;
+        } else {
+            menor = x;
+        }
+    }
+
+    int multMaiorMenor = maior * menor;
+
+    printf("Maior foi %d e o menor %d, multiplicando-os resultará em %d.\n", multMaiorMenor);
+
+    return 0;
+}
