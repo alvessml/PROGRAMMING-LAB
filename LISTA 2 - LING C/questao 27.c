9b2bd0264fd7fd4123cdc4ef48b0dc5ab3354791
Samuel Alves <alvess7221@gmail.com>
2024-08-21 04:35:22 -0300

    commit 42 - 21/08/2024

--- LISTA 2 - LING C/questao 27.c
+++ LISTA 2 - LING C/questao 27.c
@@ -1,28 +1,41 @@
 #include <stdio.h>
+#include <limits.h>
 
 int main(){
-    int x = 0;
+    int x;
+    int maior = INT_MIN;
+    int menor = INT_MAX;
 
-    int maior = 0;
-    int menor = 0;
     while(1){
         printf("Digite um número qualquer (obs: Para sair digite um número negativo par): ");
-        scanf("%d", & x);
+        scanf("%d", &x);
+        
+        // Ignora números ímpares negativos
+        if(x % 2 == 1 && x < 0){
+            continue;
+        }
 
-        if(x < 0 && x % 2 == 0){
-            break;  
+        // Sai do loop se o número for par e negativo
+        if(x % 2 == 0 && x < 0){
+           break;  
         }
         
+        // Atualiza o maior e menor
         if(x > maior){
             maior = x;
-        } else {
+        }
+        
+        if(x < menor){
             menor = x;
         }
     }
+    
+    printf("Maior número: %d\n", maior);
+    printf("Menor número: %d\n", menor);
 
     int multMaiorMenor = maior * menor;
 
-    printf("Maior foi %d e o menor %d, multiplicando-os resultará em %d.\n", multMaiorMenor);
+    printf("Maior foi %d e o menor %d, multiplicando-os resultará em %d.\n", maior, menor, multMaiorMenor);
 
     return 0;
 }
