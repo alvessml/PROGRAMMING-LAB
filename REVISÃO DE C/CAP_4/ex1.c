8fec317a57f6044934bd037785c9f6157a3a27b7
Samuel Alves <alvess7221@gmail.com.br>
2024-09-02 10:51:37 -0300

    commit aula - 02/09/2024

--- REVISÃO DE C/CAP_4/ex1.c
+++ REVISÃO DE C/CAP_4/ex1.c
@@ -1,3 +1,29 @@
 #include <stdio.h>
 
 
+/* Matriz
+- Coleção de variáveis do mesmo tipo.
+
+-- Matriz Unidimensionais
+Ex: <tipo> nome_identificador[tamanho];
+
+-- Matriz Bidimensioanis
+Ex: <tipo> nome_identificador[tamanho1][tamanho2];
+*/
+
+
+/*Funções de manipulação de strings
+
+strcopy(s1, s2) -> copia s2 em s1
+strcat(s1, s2) -> concatena(junta) s2 ao final de s1
+strlen(s1)  -> retorna o tamanho de s1
+etc...
+*/
+
+
+/*Função gets()
+- Sua forma geral é: gets (nome_da_string); Obs.: A função
+scanf() finaliza a leitura de uma string quando encontra o 
+caracter ' ' ou o '\n', já a função gets() finaliza a 
+leitura de uma string apenas quando encontra o caracter '\n'.
+*/
---
+++ REVISÃO DE C/CAP_5/ex1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/* Ponteiros
+
+Razões p/ usar:
+- Permitem mudar os argumentos das funções;
+- Manipular as rotinas de alocação dinâmica;
+- Aumentar a eficiência do programa.
+
+-- Definição
+- Ponteiro é uma variável que contém um endereço de memória. 
+- O conteúdo dessa variável é a posiçã de outra variável na memória.
+- Assim o ponteiro aponta para outra variável quando contém o endereço desta.
+
+-- Sintaxe:
+<tipo> *nome_identificador;
+
+-- Inicializar:
+<nome_identificador> = NULL;
+*/
