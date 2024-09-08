f0acd6db4b2284070e07025ca1aec7d1618c0c45
Samuel Alves <alvess7221@gmail.com.br>
2024-09-08 15:47:01 -0300

    commit 46 - 08/09/2024

--- REVISÃO DE C/CAP_7/ex1.c
+++ REVISÃO DE C/CAP_7/ex1.c
@@ -16,22 +16,25 @@ Em C há 5 tipos de DADOS definidos pelo usuário:
 É uma coleção de variáveis referenciadas por um nome, 
 fornecendo uma maneira conveniente de se ter informações 
 relacionadas agrupadas.
+
+ex:
+// struct cadastro{
+//     char nome[30];
+//     int idade;
+// };
+
+// int main(){
+//     struct cadastro pessoa;
+
+//     printf("Digite seu nome: ");
+//     scanf("%s", pessoa.nome);
+
+//     printf("Digite quantos anos você tem: ");
+//     scanf("%d", & pessoa.idade);
+
+//     printf("Seu nome é %s e você tem %d anos de idade.\n", pessoa.nome, pessoa.idade);
+//     return 0;
+// }
 */
 
-struct cadastro{
-    char nome[30];
-    int idade;
-};
-
-int main(){
-    struct cadastro pessoa;
-
-    printf("Digite seu nome: ");
-    scanf("%s", pessoa.nome);
-
-    printf("Digite quantos anos você tem: ");
-    scanf("%d", & pessoa.idade);
-
-    printf("Seu nome é %s e você tem %d anos de idade.\n", pessoa.nome, pessoa.idade);
-    return 0;
-}
+// PAREI NDA PÁGINA 11/12 de 32 do capítulo 7.
